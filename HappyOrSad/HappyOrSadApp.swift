6a771df723b855bf11351fa31245051b0ab7bfd9
Judy <jyu@lcs.on.ca>
2021-12-06 12:53:44 -0500

    Initial Commit

---
+++ HappyOrSad/HappyOrSadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyOrSadApp.swift
+//  HappyOrSad
+//
+//  Created by Judy Yu on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct HappyOrSadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
